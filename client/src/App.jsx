acf0f33cb8c1f2db93f293ac783e79b2db081c59
NitroRonak <ronaks0988@gmail.com>
2024-06-13 23:42:53 +0530

    Routing Setup Done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,38 @@
-import React,{lazy} from 'react'
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import React, { lazy } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ProtectedRoute from "./components/auth/ProtectedRoute";
 
-
-const Home = lazy(() => import('./pages/Home'))
-const Login = lazy(() => import('./pages/Login'))
-const Chat = lazy(() => import('./pages/Chat'))
-const Groups = lazy(() => import('./pages/Groups'))
-
+const NotFound = lazy(() => import("./pages/NotFound"));
+const Home = lazy(() => import("./pages/Home"));
+const Login = lazy(() => import("./pages/Login"));
+const Chat = lazy(() => import("./pages/Chat"));
+const Groups = lazy(() => import("./pages/Groups"));
 
 const App = () => {
+  let user = true;
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/chat/:chatId" element={<Chat />} />
-        <Route path="/groups" element={<Groups />} />         
-        <Route path="/login" element={<Login />} />
+
+        <Route element={<ProtectedRoute user={user} />}>
+          <Route path="/" element={<Home />} />
+
+          <Route path="/chat/:chatId" element={<Chat />} />
+
+          <Route path="/groups" element={<Groups />} />
+
+        </Route>
+
+
+        <Route path="/login" element={<ProtectedRoute user={!user} redirect="/" >
+          <Login />
+          </ProtectedRoute>
+        } />
+
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
-  )
-}
+  );
+};
 
-export default App
+export default App;
