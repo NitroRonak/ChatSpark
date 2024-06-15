747048ae94e8c47d9d468177c89dcd9e5e389c0a
NitroRonak <ronaks0988@gmail.com>
2024-06-15 14:19:19 +0530

    Loaders UI Done

--- client/src/components/layout/Loaders.jsx
+++ client/src/components/layout/Loaders.jsx
@@ -12,12 +12,12 @@ export const LayoutLoader = () => {
         }}
         height={"100%"}
       >
-        <Skeleton variant="rectangular" height={"100vh"} />
+        <Skeleton variant="rectangular" height={"100%"} />
       </Grid>
 
       <Grid item xs={12} sm={8} md={5} lg={6} height={"100%"}>
         <Stack spacing={"1rem"}>
-          {Array.from({ length: 10 }).map((_, i) => (
+          {Array.from({ length: 6 }).map((_, i) => (
             <Skeleton key={i} variant="rounded" height={"5rem"} />
           ))}
         </Stack>
@@ -33,7 +33,7 @@ export const LayoutLoader = () => {
           padding: "2rem",
         }}
       >
-        <Skeleton variant="rectangular" height={"100vh"} />
+        <Skeleton variant="rectangular" height={"100%"} />
       </Grid>
     </Grid>
   );
